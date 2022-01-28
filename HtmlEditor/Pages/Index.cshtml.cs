5588c101d6f3efb177ea1b18ad87cedf77e8dcde
Razztha <razztha111@gmail.com>
2022-01-28 23:13:17 +0530

    Test

---
+++ HtmlEditor/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HtmlEditor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string PersonalDetails { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+        
+        public void OnPostSubmit(string personalDetails)
+        {
+            this.PersonalDetails = personalDetails;
+        }
+    }
+}
